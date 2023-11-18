5f83313b62f71fe8e895f7e05fcc878a8640e805
tak-solder <tak.solder@gmail.com>
2023-11-18 14:43:09 +0900

    manifestファイルをtsに変更

---
+++ manifest.ts
@@ -0,0 +1,25 @@
+import { defineManifest } from '@crxjs/vite-plugin'
+// @ts-ignore
+import packageJson from './package.json'
+import {ConfigEnv} from "vite";
+const { version } = packageJson
+
+// Convert from Semver (example: 0.1.0-beta6)
+const [major, minor, patch, label = '0'] = version
+  // can only contain digits, dots, or dash
+  .replace(/[^\d.-]+/g, '')
+  // split into version parts
+  .split(/[.-]/)
+
+export default defineManifest(async (env: ConfigEnv) => ({
+  manifest_version: 3,
+  name:
+    env.mode === 'staging'
+      ? '[INTERNAL] Public Repo Alert'
+      : 'Public Repo Alert',
+  // up to four numbers separated by dots
+  version: `${major}.${minor}.${patch}.${label}`,
+  // semver is OK in "version_name"
+  version_name: version,
+  action: { default_popup: 'index.html' },
+}))
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
+import {ConfigEnv, defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json'
+import {crx, defineManifest} from '@crxjs/vite-plugin'
+// @ts-ignore
+import manifest from './manifest'
 
-export default defineConfig({
+export default defineConfig(async (env: ConfigEnv) => ({
   plugins: [
     react(),
     crx({ manifest }),
   ],
-})
+}))
