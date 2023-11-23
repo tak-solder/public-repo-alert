d14229be3fab57148373aced3756c8733d9928e1
tak-solder <tak.solder@gmail.com>
2023-11-23 16:25:44 +0900

    公開中のリポジトリのみ通知を出すようにする

--- src/content-script/App.tsx
+++ src/content-script/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import {BottomNotification} from "./BottomNotification.tsx";
+import {OctolyticsProvider} from "./octolytics.tsx";
 
 export const App: React.FC = () => (
-  <>
+  <OctolyticsProvider>
     <BottomNotification />
-  </>
+  </OctolyticsProvider>
 );
---
+++ src/content-script/octolytics.tsx
@@ -0,0 +1,93 @@
+import {createContext, FC, ReactNode, useContext, useState} from "react";
+import {useMutationObserver} from "ahooks";
+
+export type Octolytics = {
+  repositoryName?: string;
+  repositoryIsPublic?: boolean;
+};
+
+const octolyticsKeyToMetaName: Record<keyof Octolytics, string> = {
+  repositoryName: 'octolytics-dimension-repository_nwo',
+  repositoryIsPublic: 'octolytics-dimension-repository_public',
+} as const;
+
+const octolyticsMetaNames = Object.values(octolyticsKeyToMetaName);
+
+const getOctolytics = () => {
+  console.log('getOctolytics');
+  const octolyticsTags = Array.from(document.querySelectorAll('meta[name*=octolytics-]'));
+  const octolyticsMap = new Map(octolyticsTags.map(octolytics => [octolytics.getAttribute('name'), octolytics.getAttribute('content')]));
+
+  const octolytics: Octolytics = {};
+  if (octolyticsMap.has(octolyticsKeyToMetaName.repositoryName)) {
+    octolytics.repositoryName = octolyticsMap.get(octolyticsKeyToMetaName.repositoryName) as string;
+  }
+  if (octolyticsMap.has(octolyticsKeyToMetaName.repositoryIsPublic)) {
+    octolytics.repositoryIsPublic = octolyticsMap.get(octolyticsKeyToMetaName.repositoryIsPublic) === 'true';
+  }
+
+  return octolytics;
+}
+
+const OctolyticsContext = createContext<Octolytics>({});
+
+type Props = {
+  children: ReactNode;
+}
+
+const isOctolyticsMetaTag = (node: HTMLElement) => {
+  if (node.tagName.toLowerCase() !== 'meta') {
+    return false;
+  }
+  const name = node.getAttribute('name');
+  return name && octolyticsMetaNames.includes(name);
+}
+
+export const OctolyticsProvider: FC<Props> = ({children}) => {
+  const [octolytics, setOctolytics] = useState<Octolytics>(getOctolytics);
+
+  useMutationObserver(
+    (mutations) => {
+      // 追加された要素の中にOctolyticsで使っているmetaタグが存在するか
+      const addedTagExists = !!mutations
+        .find(mutation => {
+          if (!mutation.addedNodes.length) {
+            return false;
+          }
+
+          return (Array.from(mutation.addedNodes) as HTMLElement[])
+            .find(isOctolyticsMetaTag)
+        });
+
+      // 削除された要素の中にOctolyticsで使っているmetaタグが存在するか
+      const removedTagExists = !!mutations
+        .find(mutation => {
+          if (!mutation.removedNodes.length) {
+            return false;
+          }
+
+          return (Array.from(mutation.removedNodes) as HTMLElement[])
+            .find(isOctolyticsMetaTag)
+        });
+
+
+      // mutationListのaddNodesまたはremovedNodesにOctolyticsで使っているmetaタグが含まれていたら再取得する
+      if (addedTagExists || removedTagExists) {
+        setOctolytics(getOctolytics());
+      }
+    },
+    document.querySelector('head') as HTMLHeadElement,
+    {
+      subtree: true,
+      childList: true,
+      attributeFilter: ['value']
+    }
+  );
+  console.log(JSON.stringify(octolytics, null, 2));
+
+  return <OctolyticsContext.Provider value={octolytics}>
+    {children}
+  </OctolyticsContext.Provider>;
+}
+
+export const useOctolytics: () => Octolytics = () => useContext(OctolyticsContext);
