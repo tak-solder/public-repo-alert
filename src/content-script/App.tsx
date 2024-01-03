dadf8bbb865d634b1aaa5d4baac2369d037b878a
tak-solder <tak.solder@gmail.com>
2024-01-03 16:42:23 +0900

    コメント入力前に確認するように変更

--- src/content-script/App.tsx
+++ src/content-script/App.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import {BottomNotification} from "./BottomNotification.tsx";
 import {OctolyticsProvider} from "./octolytics.tsx";
-import {PublicRepositoryObserveFeature} from "./PublicRepositoryObserveFeature.tsx";
+import {PublicRepositoryFormObserver} from "./PublicRepositoryFormObserver.tsx";
 
 export const App: React.FC = () => (
   <OctolyticsProvider>
-    <PublicRepositoryObserveFeature/>
+    <PublicRepositoryFormObserver/>
     <BottomNotification />
   </OctolyticsProvider>
 );
